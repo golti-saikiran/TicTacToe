c5e51abf914b89c85514952fc75b4bc71f159966
golti-saikiran <saikiran.golti@gmail.com>
2025-01-30 19:57:49 +0530

    player score text alignment issue fixed

--- src/components/ScoreBoard.jsx
+++ src/components/ScoreBoard.jsx
@@ -2,16 +2,16 @@ const ScoreBoard = ({ isAutoPlay, setIsAutoPlay, xScore, oScore, restartGame }) 
 
   return (
     <>
-      <div>
+      <div className="checkbox">
         <input type="checkbox" checked={isAutoPlay} id="autoPlayCheckBox" onChange={() => setIsAutoPlay(!isAutoPlay)} />
         <label htmlFor="autoPlayCheckBox"> Play with Computer</label>
       </div>
       <div className='scorecard'>
         <span className="player">{`X - ${xScore}`}</span>
         <span className="player">{`O - ${oScore}`}</span>
-        {xScore >= 3 || oScore >= 3 ?
-          <button className='btn' onClick={() => restartGame()}>Restart</button> : ""}
       </div>
+      {xScore >= 3 || oScore >= 3 ?
+          <button className='btn' onClick={() => restartGame()}>Restart</button> : ""}
     </>
   )
 }
